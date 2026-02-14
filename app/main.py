bebfed4ce5b4f9029187d09b0604ff1b814bbdb0
Farha2318 <farhanabegam501@gmail.com>
2026-02-14 21:49:24 +0530

    Auth module base structure created

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.routes import auth_routes
 
 app = FastAPI(title="Crypto Exchange - User Module")
+
+app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
 
 @app.get("/")
 async def root():
